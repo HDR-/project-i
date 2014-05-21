d026b683dda5399dd2d8fbf5be2de3c8e549e4ec
HDR- <heng.du.r@gmail.com>
2014-05-20 23:48:41 -0400

    about me panel, page size and overlay

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('#about-me').click(function(e) {
+        e.stopPropagation();
+        $('#full-page-overlay').css('display', 'block');
+        $('#about-me-panel').show(400);
+    });
+    
+    $('html').click(function() {
+        $('#about-me-panel').hide(400);
+        $('#full-page-overlay').css('display', 'none');
+    });
+});
